b0fc418a1c5ace4146aa9c4dd9918432e5666037
Will <111468709+yurabinsuki@users.noreply.github.com>
2026-03-26 20:31:35 -0300

    Basic Menu Structure

---
+++ Assets/Scripts/Utils/ButtonExit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ButtonExit : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
